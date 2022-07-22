e70b6ece485f7ca849105d2ea81bdff3eed6ae62
Rodrigo Lopez <rodrigo.lapidus@gmail.com>
2022-07-22 16:35:24 +0200

    upload of an introductory exercise to JS

---
+++ Week 1/day5/checkAge.js
@@ -0,0 +1,7 @@
+const age = parseInt(prompt("Welcome to the site. What is your age?"))
+if (age >= 21) {
+    console.log("Get drunk tonight, baby")
+}
+else {
+    console.log("You are below the legal drinking age")
+}
