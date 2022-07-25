8c2dd1c66ea7b8b9cc04c98d1644af6605eaec1e
Rodrigo Lopez <rodrigo.lapidus@gmail.com>
2022-07-25 12:00:42 +0200

    upload of exercise about variables in JavaScript from day 1 of week 2

---
+++ Week 2/day1/biography.js
@@ -0,0 +1,9 @@
+let name = "Rodrigo";
+let surname = "Lopez";
+let birthDate = "14/05/1991";
+let hobby1 = "Snowboarding";
+let hobby2 = "Reading";
+let hobby3 = "Travelling";
+let education = "Mechanical Engineering";
+
+console.log(`${name} ${surname} was born ${birthDate}. His passions are ${hobby1}, ${hobby2} and ${hobby3}. In university he studied ${education}. He still has ${80-[2022-birthDate.substring(6,)]} years before turning 80.`);
